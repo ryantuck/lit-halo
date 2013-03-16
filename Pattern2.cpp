5a0dd5cb59c912eb197fe55cbed206553711fa17
ryantuck <ryanstephentuck@gmail.com>
2013-03-16 19:33:23 -0400

    Added files
    Had to move pattern2 because I had put it in the wrong folder. Also
    created Pattern3 file while testing a custom template in Xcode.

---
+++ Pattern2.cpp
@@ -0,0 +1,15 @@
+//
+//  Pattern2.cpp
+//  LIT-Source
+//
+//  Created by Ryan Tuck on 3/15/13.
+//  Copyright (c) 2013 Ryan Tuck. All rights reserved.
+//
+
+#include "Pattern2.h"
+
+
+Pattern2::Pattern2()
+{
+	createSequencesArray(1);
+}
---
+++ Pattern2.h
@@ -0,0 +1,21 @@
+//
+//  Pattern2.h
+//  LIT-Source
+//
+//  Created by Ryan Tuck on 3/15/13.
+//  Copyright (c) 2013 Ryan Tuck. All rights reserved.
+//
+
+#ifndef __LIT_Source__Pattern2__
+#define __LIT_Source__Pattern2__
+
+#include "Pattern.h"
+#include "CustomSequences.h"
+
+class Pattern2 : public Pattern
+{
+public:
+	Pattern2();
+};
+
+#endif /* defined(__LIT_Source__Pattern2__) */
