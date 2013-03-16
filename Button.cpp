ae0de18548aae3c825d9e276ed22697d119f60c2
Danny Kern <dan.jkern@gmail.com>
2013-03-15 19:13:35 -0700

    no need for debounce on buttons, simplified code
    pressed is 1 when button is pressed
    released is 1 when button is released
    when there is no state change, both are 0

--- Button.cpp
+++ Button.cpp
@@ -9,7 +9,10 @@
 
 Button::Button()
 {
-	debounceDelay = 20; //ms
+    lastButtonState = 1;
+    
+    pressed = 0;
+    released = 0;
 }
 
 Button::Button(int newPin)
@@ -17,63 +20,29 @@ Button::Button(int newPin)
 	pin = newPin;
 	pinMode(pin, INPUT);
 	digitalWrite(pin, HIGH);
-    
-    debounceDelay = 20; //ms
+
+    lastButtonState = 1;
 }
 
 void Button::checkState()
 {
-//	state	= digitalRead(pin);
-//	pressed = 0;
-//	
-//	if (pressedCounter == 0)
-//	{
-//		if (state == LOW)
-//		{
-//			pressed = 1;
-//			pressedCounter = 1;
-//		}
-//	}
-//	else
-//	{
-//		pressedCounter++;
-//		if (pressedCounter == 30)
-//			pressedCounter = 0;
-//	}
-//	
-//	lastState = state;
     
     bool reading = digitalRead(pin);
     
-    //if the switch changed due to noise or pressing
-    if(reading != lastButtonState)
+    if(reading != lastButtonState) //could be due to noise or pressing
     {
-        //reset debounce time
-        lastDebounceTime = millis();
-
-    }
-    
-    if(abs(millis() - lastDebounceTime) > debounceDelay)
-    {
-        buttonState = reading;
-    }
-    
-    //detect high to low transition
-    if(lastButtonState == 1 && buttonState == 0)
-    {
-        pressed = 1;
-        Serial.println("Button has been pressed");
+        if(!reading) pressed = 1;
+        if(reading) released = 1;
     }
     else
     {
         pressed = 0;
+        released = 0;
     }
-    
     lastButtonState = reading;
-	
+    
+    if(pressed) Serial.println("button pressed");
+    if(released) Serial.println("button released");
 }
-
-
-
 
 
