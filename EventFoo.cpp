ee44403842fd0fa1e1c5100ecd5ac5859112f249
ryantuck <ryanstephentuck@gmail.com>
2013-08-13 14:30:53 -0400

    EventFoo converted to list behavior

--- EventFoo.cpp
+++ EventFoo.cpp
@@ -12,26 +12,25 @@ EventFoo::EventFoo()
 {
 	killCounter = 0;
 	
-	addItem();
-	foos[0]->setBlock(LITColor.red, maxBrightness, 2, 5);
+//	addItem();
+//	foos[0]->setBlock(LITColor.red, maxBrightness, 2, 5);
+	
+	Foo* aFoo = new Foo;
+	addFoo(aFoo);
+	aFoo->addLEDs(LITColor.red, maxBrightness, 2, 5);
 }
 
 void EventFoo::update()
 {
-	if (foos)
+	if (hasFoos())
 	{
-		updateFoos();
-		updateLEDs();
 		killCounter++;
 		Serial.println(killCounter);
 		
 		if (killCounter > 60)
-			foos[0]->readyToDie = 1;
-		
-		
-		if (foos[0]->readyToDie)
-			destroyArray();
+			myFooList.entry(0)->me->readyToDie = 1;
+		if (myFooList.entry(0)->me->readyToDie)
+			myFooList.removeAllEntries();
 	}
-	
 }
 
