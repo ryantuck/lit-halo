3d3d19ab4a8e0a398ecd82e2bd3be92fda2f8415
ryantuck <ryanstephentuck@gmail.com>
2013-08-13 14:46:00 -0400

    Converted multiple bouncing foos to list behavior

--- MultipleBouncingFoos.cpp
+++ MultipleBouncingFoos.cpp
@@ -9,30 +9,31 @@
 #include "MultipleBouncingFoos.h"
 
 MultipleBouncingFoo::MultipleBouncingFoo()
-{
+{	
 	for (int n=0;n<4;n++)
 	{
-		addItem();
-		foos[n]->setBlock(*LITColor.colorList[n], maxBrightness, n*8, n*8);
-		foos[n]->direction = rand()%2;
-		foos[n]->period = n+1;
+		Foo* aFoo = new Foo;
+		aFoo->addLEDs(*LITColor.colorList[n], maxBrightness, n*8, n*8);
+		aFoo->direction = rand()%2;
+		aFoo->period = n+1;
+		addFoo(aFoo);
 	}
 }
 
 
 void MultipleBouncingFoo::update()
 {	
-	for (int n=0;n<numberOfFoos;n++)
+	for (int n=0;n<countFoos();n++)
 	{
-		int	addr1 = foos[n]->fLEDs[0]->address;
-		bool dir1 = foos[n]->direction;
-		int	   p1 = foos[n]->period;
+		int addr1 = myFooList.entry(n)->me->myLEDList.entry(0)->me->address;
+		bool dir1 = myFooList.entry(n)->me->direction;
+		int p1	  = myFooList.entry(n)->me->period;
 		
-		for (int p=n+1;p<numberOfFoos;p++)
+		for (int p=n+1;p<countFoos();p++)
 		{
-			int addr2 = foos[p]->fLEDs[0]->address;
-			bool dir2 = foos[p]->direction;
-			int	   p2 = foos[p]->period;
+			int addr2 = myFooList.entry(p)->me->myLEDList.entry(0)->me->address;
+			bool dir2 = myFooList.entry(p)->me->direction;
+			int p2	  = myFooList.entry(p)->me->period;
 			
 			if (addr1 < 2 && addr2 > 29) addr1 += 32;
 			if (addr2 < 2 && addr1 > 29) addr2 += 32;
@@ -44,24 +45,23 @@ void MultipleBouncingFoo::update()
 				{
 					if (p1 > p2)
 					{
-						foos[p]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
-						
+						myFooList.entry(p)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 					else if (p1 < p2)
 					{
-						foos[n]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
+						myFooList.entry(n)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 				}
 				else				//	different direction
 				{
-					foos[n]->switchDirection();
-					foos[p]->switchDirection();
-					foos[n]->period = p2;
-					foos[p]->period = p1;
+					myFooList.entry(n)->me->switchDirection();
+					myFooList.entry(p)->me->switchDirection();
+					myFooList.entry(n)->me->period = p2;
+					myFooList.entry(p)->me->period = p1;
 				}
 			}
 			else if (addr1+1 == addr2 || addr1-1 == addr2)	// adjacent
@@ -72,15 +72,15 @@ void MultipleBouncingFoo::update()
 				{
 					if (p1 > p2)
 					{
-						foos[p]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
+						myFooList.entry(p)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 					else if (p1 < p2)
 					{
-						foos[n]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
+						myFooList.entry(n)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 				}
 				
@@ -88,26 +88,26 @@ void MultipleBouncingFoo::update()
 				{
 					if (dir1)
 					{
-						foos[n]->switchDirection();
-						foos[p]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
+						myFooList.entry(n)->me->switchDirection();
+						myFooList.entry(p)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 				}
 				else if (addr1 > addr2)		// different directions
 				{
 					if (dir2)
 					{
-						foos[n]->switchDirection();
-						foos[p]->switchDirection();
-						foos[n]->period = p2;
-						foos[p]->period = p1;
+						myFooList.entry(n)->me->switchDirection();
+						myFooList.entry(p)->me->switchDirection();
+						myFooList.entry(n)->me->period = p2;
+						myFooList.entry(p)->me->period = p1;
 					}
 				}
 			}
 			else if (addr1 + 2 == addr2 || addr1 - 2 == addr2)
 			{
-				if (foos[n]->canUpdate() && foos[p]->canUpdate())
+				if (myFooList.entry(n)->me->canUpdate() && myFooList.entry(p)->me->canUpdate())
 				{
 					if (dir1 != dir2)
 					{
@@ -115,14 +115,14 @@ void MultipleBouncingFoo::update()
 						{
 							if (dir1)
 							{
-								foos[p]->periodCounter = 0;
+								myFooList.entry(p)->me->periodCounter = 0;
 							}
 						}
 						else if (addr1 > addr2)
 						{
 							if (dir2)
 							{
-								foos[n]->periodCounter = 0;
+								myFooList.entry(n)->me->periodCounter = 0;
 							}
 						}
 					}
