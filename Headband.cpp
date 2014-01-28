61ea9f0f79059468a98f29fc9ddbb19cea56913f
ryantuck <ryanstephentuck@gmail.com>
2014-01-28 18:06:52 -0500

    simpler boolean button check for simulator/headband
    added a boolean value ‘hardware’ that defines whether hardware (real
    headband) or simulator is in place. Don’t need to comment out button
    checks anymore.

--- Headband.cpp
+++ Headband.cpp
@@ -11,7 +11,7 @@ Headband::Headband()
 	downButton	= Button(5);
 	upButton	= Button(6);
 	
-	fooManager = new ChristmasManager();
+	fooManager = new NonAudioManager();
 }
 
 void Headband::update()
@@ -23,8 +23,12 @@ void Headband::update()
 
 void Headband::checkButtons()
 {
-//    upButton.checkState();
-//	downButton.checkState();
+	bool hardware = false;
+	if (hardware)
+	{
+		upButton.checkState();
+		downButton.checkState();
+	}
 	
 	if (upButton.pressed)
 	{
