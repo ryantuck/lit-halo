ab73f81ca57850c20d5e88d0af3c4b79f5b473bb
Danny Kern <dan.jkern@gmail.com>
2014-03-02 14:54:54 -0800

    edited matlab analyzer code
    uses midpoint algo

--- support/data/analyzer.m
+++ support/data/analyzer.m
@@ -5,9 +5,9 @@ fs = 1/50;
 
 x = 0:fs:(length(data(:,1))-1)*fs;
 
-y1 = data(:,1);
-y2 = data(:,2);
-y3 = data(:,3);
+y3 = data(:,1);
+y1 = data(:,2);
+y2 = data(:,3);
 y4 = data(:,4);
 y5 = data(:,5);
 y6 = data(:,6);
@@ -40,17 +40,31 @@ y7 = data(:,7);
 % end
     
     
-N = 100;
-threshold(1:N-1) = 0;
-threshold2(1:N-1) = 0;
-threshold3(1:N-1) = 0;
-
-
+N = 50;
+threshold6(1:N-1) = 0;
+deriv(1:N-1) = 0;
+beatDetect(i:N-1) = 0;
+% threshold(1:N-1) = 0;
+% threshold2(1:N-1) = 0;
+% threshold3(1:N-1) = 0;
+% 
+% 
 for i = 1:length(x) - N +1
-    [threshold(i+N-1), finalD2] = kmean_dk(y2(i:i+N-1));
-    [IDX, c] = kmeans(y2(i:i+N-1), 2, 'start', 'uniform');
-    threshold2(i+N-1) = mean(c);
-    threshold3(i+N-1) = mean(y2(i:i+N-1));
+%     [threshold(i+N-1), finalD2] = kmean_dk(y2(i:i+N-1));
+%     [IDX, c] = kmeans(y2(i:i+N-1), 2, 'start', 'uniform');
+%     threshold2(i+N-1) = mean(c);
+%     threshold3(i+N-1) = mean(y2(i:i+N-1));
+      threshold6(i+N-1) = (max(y2(i:i+N-1)) + min(y2(i:i+N-1)))/2;
+      deriv(i+N-1) = y2(i+N-1) - y2(i+N-2);
+      if(deriv(i+N-1) < 0) 
+          deriv(i+N-1) = 0;
+      end
+      threshold7(i+N-1) = (max(deriv(i:i+N-1)) + min(deriv(i:i+N-1)))/2;
+      if(deriv(i+N-1) > threshold7(i+N-1) && y2(i+N-1) > threshold6(i+N-1) && deriv(i+N-1) > 80)
+          beatDetect(i+N-1) = 150;
+      else
+          beatDetect(i+N-1) = 0;
+      end
 end
 
-plot(x, y2, x, threshold, x, threshold2, x, threshold3)
+plot(x, y2, x, threshold6, x, deriv, x, threshold7, x, beatDetect, '.')
